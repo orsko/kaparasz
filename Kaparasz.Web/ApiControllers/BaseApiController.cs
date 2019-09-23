d4389d0f6c930147753387a78d3436befc514df2
Ors Kovacs <orskovacs92@gmail.com>
2019-09-23 15:25:01 +0200

    base api

---
+++ Kaparasz.Web/ApiControllers/BaseApiController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kaparasz.Web.ApiControllers
+{
+    [Produces("application/json")]
+    public abstract class BaseApiController: Controller
+    {
+        protected JsonResult JsonSuccess()
+        {
+            return Json(new { success = true });
+        }
+
+        protected JsonResult JsonSuccess<T>(T data)
+        {
+            return Json(new { success = true, data });
+        }
+
+        protected JsonResult JsonError(string error)
+        {
+            return Json(new { success = false, error });
+        }
+    }
+}
